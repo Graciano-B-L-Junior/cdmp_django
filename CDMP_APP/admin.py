56d3b2ff84cdbe9b9587feedb5321e12578168ec
Graciano-B-L-Junior <gracianobarbosalimajunior@gmail.com>
2023-08-05 18:10:22 -0300

    Mudanca no banco de dados

--- CDMP_APP/admin.py
+++ CDMP_APP/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Categoria,Cliente,Depositos,Gasto,HistoricoModificaoes,MetaFinanceira
+from .models import Categoria,Cliente,Depositos,Gasto,MetaFinanceira,HistoricoCliente
 
 
 class ClienteAdmin(admin.ModelAdmin):
-    list_display=["nome","email","gastos","metas","depositos","foto"]
+    list_display=["nome","email","foto"]
     
 
 
@@ -13,5 +13,5 @@ admin.site.register(Categoria)
 admin.site.register(Cliente,ClienteAdmin)
 admin.site.register(Depositos)
 admin.site.register(Gasto)
-admin.site.register(HistoricoModificaoes)
+admin.site.register(HistoricoCliente)
 admin.site.register(MetaFinanceira)
--- CDMP_APP/models.py
+++ CDMP_APP/models.py
@@ -2,48 +2,67 @@ from django.db import models
 from datetime import datetime
 from django.utils import timezone
 # Create your models here.
-
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    foto = models.CharField(default="default.jpg",blank=True,null=True,max_length=100)
+    def __str__(self) -> str:
+        return self.nome
+    class Meta:
+        verbose_name="Cliente"
+        verbose_name_plural="Clientes"
 class Categoria(models.Model):
     nome = models.CharField(unique=True,max_length=50)
     data_criacao = models.DateTimeField(default=timezone.now)
+    class Meta:
+        verbose_name="Categoria"
+        verbose_name_plural="Categorias"
 
+    def __str__(self) -> str:
+        return self.nome
 class Gasto(models.Model):
     valor = models.FloatField()
     descricao = models.CharField(max_length=50)
-    categoria = models.ForeignKey(Categoria,null=True,blank=True,on_delete=models.SET_NULL)
     data_gasto = models.DateTimeField(default=timezone.now)
+    cliente = models.ForeignKey(Cliente,on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria,null=True,on_delete=models.SET_NULL)
+    class Meta:
+        verbose_name="Gasto"
+        verbose_name_plural="Gastos"
+
+    def __str__(self) -> str:
+        return self.descricao
+    
 
 class Depositos(models.Model):
     valor = models.FloatField()
     descricao = models.CharField(max_length=50)
     data_deposito = models.DateTimeField(default=timezone.now)
-
+    cliente = models.ForeignKey(Cliente,on_delete=models.CASCADE)
     class Meta:
         verbose_name="Deposito"
         verbose_name_plural="Depositos"
+
+    def __str__(self) -> str:
+        return self.descricao
+
+
 
 class MetaFinanceira(models.Model):
     nome_meta = models.CharField(max_length=50)
     valor_atual = models.FloatField()
     icone = models.CharField(max_length=50)
     valor_total = models.FloatField()
+    cliente = models.ForeignKey(Cliente,on_delete=models.CASCADE)
 
-class Cliente(models.Model):
-    nome = models.CharField(max_length=100)
-    email = models.EmailField()
-    gastos = models.ForeignKey(Gasto,blank=True,null=True,on_delete=models.SET_NULL)
-    depositos = models.ForeignKey(Depositos,blank=True,null=True,on_delete=models.SET_NULL)
-    metas = models.ForeignKey(MetaFinanceira,blank=True,null=True,on_delete=models.SET_NULL)
-    foto = models.CharField(default="default.jpg",blank=True,null=True,max_length=100)
+    def __str__(self) -> str:
+        return self.nome_meta
 
+class HistoricoCliente(models.Model):
+    cliente = models.ForeignKey(Cliente,on_delete=models.CASCADE)
+    operacao = models.CharField(max_length=100)
+    data_operacao = models.DateTimeField()
     class Meta:
-        verbose_name="Cliente"
-        verbose_name_plural="Clientes"
-
-class HistoricoModificaoes(models.Model):
-    gasto_fk = models.ForeignKey(Gasto,on_delete=models.CASCADE)
-    data_alteracao =models.DateTimeField(default=timezone.now)
-    nome = models.CharField(max_length=50)
-    categoria = models.ForeignKey(Categoria,on_delete=models.SET_NULL,null=True)
-    valor = models.FloatField()
+        verbose_name="HistoricoCliente"
+        verbose_name_plural="HistoricoClientes"
     
