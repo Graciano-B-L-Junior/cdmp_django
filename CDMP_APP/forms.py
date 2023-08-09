8ddebb7be0cad4ab785f8b8a7a486fc6be26a740
Graciano-B-L-Junior <gracianobarbosalimajunior@gmail.com>
2023-08-09 09:47:48 -0300

    formulario de adição de gastos funcionando

--- CDMP_APP/forms.py
+++ CDMP_APP/forms.py
@@ -1,7 +1,20 @@
 from django import forms
+from datetime import datetime
+from .models import Categoria
 
+def set_choices_categoria() -> tuple:
+        categorias = Categoria.objects.all()
+        tupla= tuple()
+        lista=[]
+        for categoria in categorias:
+              lista.append(categoria.nome)
+        tupla = ((categoria,categoria) for categoria in lista)
+        return tupla
 class GastoForm(forms.Form):
-    valor = forms.FloatField()
+    valor = forms.FloatField(error_messages={"invalid":"Preencha o campo apenas com números"})
     descricao = forms.CharField(max_length=50)
-    data_gasto = forms.DateField()
-    categoria = forms.CharField()
+    data_gasto = forms.DateField(widget=forms.DateInput(attrs={'type':'date','max':datetime.now().date}))
+
+    categoria = forms.ChoiceField(widget=forms.Select,choices=set_choices_categoria())
+
+    
--- CDMP_APP/views.py
+++ CDMP_APP/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from . import models
 from .forms import GastoForm
+
 # Create your views here.
 def index(request):
     cliente = models.Cliente.objects.all()[0]
@@ -12,7 +13,19 @@ def index(request):
 
 def add_gasto(request):
     if request.method == "POST":
-        pass
+        form = GastoForm(request.POST)
+        if form.is_valid():
+            descricao = form.cleaned_data["descricao"]
+            valor = form.cleaned_data["valor"]
+            data_gasto = form.cleaned_data["data_gasto"]
+            categoria = form.cleaned_data["categoria"]
+            cliente = models.Cliente.objects.get(nome="Graciano Junior")
+            categoria = models.Categoria.objects.get(nome=categoria)
+            Gasto = models.Gasto(valor=valor,descricao=descricao,data_gasto=data_gasto,cliente=cliente,categoria=categoria)
+            Gasto.save()
+            return HttpResponseRedirect('/')
+        else:
+            return render(request,"CDMP_APP/add_gasto.html",{"form":form,"message":"Preencha os campos corretamente"})
     else:
         form = GastoForm()
     return render(request,"CDMP_APP/add_gasto.html",{"form":form})
