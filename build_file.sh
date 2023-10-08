4cc678e88a189f18ba6a32841706c82ee48bd1f7
Graciano-B-L-Junior <gracianobarbosalimajunior@gmail.com>
2023-10-08 14:04:23 -0300

    teste deploy 4

--- build_file.sh
+++ build_file.sh
@@ -1,5 +1,5 @@
 echo "BUILD START"
 
 python3.9 -m pip install -r requirements.txt
-python3.9 -m pip install pysqlite
+python3.9 -m pip install sqlite3
 python3.9 manage.py collectstatic --noinput --clear
