ee7942da64f341609c290364033d3006a545be26
Graciano-B-L-Junior <gracianobarbosalimajunior@gmail.com>
2023-10-08 14:42:54 -0300

    teste deploy 8

--- build_file.sh
+++ build_file.sh
@@ -1,6 +1,7 @@
 echo "BUILD START"
 
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
-python3 makemigrations
-python3 migrate
+apt install libpq-dev
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+python3.9 makemigrations
+python3.9 migrate
