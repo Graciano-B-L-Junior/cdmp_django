fe792cacfefff15c5d3aa37d32f4267ef4a92e97
Graciano-B-L-Junior <gracianobarbosalimajunior@gmail.com>
2024-02-02 15:43:36 -0300

    ambiente dev feito

--- app/CDMP/settings.py
+++ app/CDMP/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c8!s*%@$lj*18-h&h73f6_p4_0rgqu=lpwb6%09s0up1owsnv8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
--- dockerfile
+++ dockerfile
@@ -11,7 +11,7 @@ RUN apt-get update && \
 
 RUN pip install -r requirements.txt && pip install psycopg
 
-RUN chmod a+x app_build.sh
+RUN chmod a+x app_build.sh && chmod a+x app_build_dev.sh
 RUN adduser --disabled-password cdmp
 RUN chown -R cdmp:cdmp /web
 USER cdmp
